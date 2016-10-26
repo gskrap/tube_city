74686a7c86201027a19a28c2f8d2f210c974ee11
George Skrapits <gskrap@gmail.com>
2016-10-26 14:51:36 -0400

    implement bootstrap and user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
+  get    'sessions/login'   => 'sessions#new'
+  post   'sessions'         => 'sessions#create'
+  delete 'sessions/destroy' => 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
