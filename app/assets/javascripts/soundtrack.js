3fec0e8988056622626390f3bbf41010acc70463
George Skrapits <gskrap@gmail.com>
2016-11-01 14:34:11 -0400

    fix counter for clip numbers on forms

--- app/assets/javascripts/soundtrack.js
+++ app/assets/javascripts/soundtrack.js
@@ -9,12 +9,18 @@ $('#soundtrack_submit').on('click', function(event){
   $('#soundtrack_container').html(
     "<iframe id='sound_vid' type='text/html' width='640' height='390' src='https://www.youtube.com/embed/" + youtube_parser($('#soundtrack_url').val()) + "?controls=1&showinfo=0&rel=0&autoplay=0&enablejsapi=1&start=0' frameborder='0' allowfullscreen></iframe>"
   );
+  $('#soundtrack_container').append(
+    "<input type='text' placeholder='Soundtrack Start Time' name='soundtrack_start'>"
+  );
   $('#number_of_clips_container').show();
 });
+
+var clipCount = 1;
 
 $('#clips_submit').on('click', function(event){
   event.preventDefault();
   $('#clip_fields_container').append(
-    "<input type='text' placeholder='Clip 1 Youtube URL' name='clip1_url'><input type='text' placeholder='Start Time in Seconds' name=clip1_start'><input type='text' placeholder='Duration in Seconds' name='clip1_duration'><br>"
+    "<input type='text' placeholder='Clip " + clipCount + " Youtube URL' name='clip" + clipCount + "_url'><input type='text' placeholder='Start Time in Seconds' name=clip" + clipCount + "_start'><input type='text' placeholder='Duration in Seconds' name='clip" + clipCount + "_duration'><br>"
   );
+  clipCount += 1;
 });
