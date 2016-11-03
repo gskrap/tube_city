87ac72c8202f77d78aae30fe59a5be6a19fdc5b2
George Skrapits <gskrap@gmail.com>
2016-11-03 16:16:53 -0400

    style create new mix page

--- app/assets/javascripts/soundtrack.js
+++ app/assets/javascripts/soundtrack.js
@@ -10,19 +10,22 @@ $('#soundtrack_submit').on('click', function(event){
     "<iframe id='sound_vid' type='text/html' width='640' height='390' src='https://www.youtube.com/embed/" + youtube_parser($('#soundtrack_url').val()) + "?controls=1&showinfo=0&rel=0&autoplay=0&enablejsapi=1&start=0' frameborder='0' allowfullscreen></iframe>"
   );
   $('#soundtrack_container').append(
-    "<br><input id='soundtrack_start' type='text' placeholder='Soundtrack Start Time' name='soundtrack_start'>"
+    "<br>Soundtrack Start Time in Seconds: <input id='soundtrack_start' type='text' value=0 name='soundtrack_start'>"
   );
-  $('#number_of_clips_container').show();
+  $('#add_clip_form').show();
 });
 
 var clipCount = 1;
 
 $('#clips_submit').on('click', function(event){
   event.preventDefault();
-  $('#clip_fields_container').append(
-    "<input class='clip_row' type='text' placeholder='Clip " + clipCount + " Youtube URL' id='clip" + clipCount + "_url' name='clip" + clipCount + "_url'><input type='text' placeholder='Start Time in Seconds' id='clip" + clipCount + "_start' name=clip" + clipCount + "_start'><input type='text' placeholder='Duration in Seconds' id='clip" + clipCount + "_duration' name='clip" + clipCount + "_duration'><br>"
-  );
+  if (clipCount <= 20) {
+    $('#clip_fields_container').append(
+      "<input class='clip_row form-control input-sm clip_field' type='text' placeholder='Clip " + clipCount + " Youtube URL' id='clip" + clipCount + "_url' name='clip" + clipCount + "_url'><input class='form-control input-sm clip_field' type='text' placeholder='Start Time in Seconds' id='clip" + clipCount + "_start' name=clip" + clipCount + "_start'><input class='form-control input-sm clip_field' type='text' placeholder='Duration in Seconds' id='clip" + clipCount + "_duration' name='clip" + clipCount + "_duration'><br>"
+    );
+  };
   clipCount += 1;
+  $('#create_mix_form').show();
 });
 
 $('#mix_submit').on('click', function(event){
